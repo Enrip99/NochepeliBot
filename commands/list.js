3eb024505818b05770ea34f274dc5ce8cdb18ffe
enrip99 <enric.1999@hotmail.com>
2022-05-02 14:40:04 +0200

    List ahora incluye numero de interesados y creador

--- commands/list.js
+++ commands/list.js
@@ -9,11 +9,20 @@ module.exports = {
 		if (!args.length) { //muestra solo la lista de películas
 			if (!lista.lista.length) message.channel.send("No hay películas en la lista")
 			else {
+				let listmsgs = []
+				let listprom = []
 				let tosend = ""
 				for (let i = 0; i < lista.lista.length; ++i){
-					tosend = tosend + "- **" + lista.lista[i].nombre + "**\n"
+					listmsgs.push("\n- **" + lista.lista[i].nombre + "** (" + lista.lista[i].interesados.length + ") - Propuesta por: **")
+					listprom.push(client.users.fetch(String(lista.lista[i].propuesto)))
 				}
+
+				Promise.all(listprom).then(values => {
+					for (let j = 0; j < values.length; ++j){
+						tosend += listmsgs[j] += values[j].username + "**"
+					}
 				message.channel.send(tosend)
+				})
 			}
 		}
 	}
