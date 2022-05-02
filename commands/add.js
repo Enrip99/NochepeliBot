014bd5a3e670e7430ed11a199bacc70129181cab
kplac <kplaceholder@gmail.com>
2022-05-02 19:42:08 +0200

    vaporeon check

--- commands/add.js
+++ commands/add.js
@@ -12,6 +12,7 @@ module.exports = {
 		}
 		else {
 			if(vaporeon_check(message)) {
+				FilmManager.instance.set_latest_film(null)
 				return
 			}
 			let inputpeli = utils.parse_film_name(message.content)
@@ -40,7 +41,10 @@ function vaporeon_check(message) {
 	let regex = /\bvaporeon\b/gmi
 	let ret = false
 	if(message.content.match(regex)) {
-		message.channel.send(utils.random_from_list(["jaja qué gracioso", "comedy heaven", "me parto los cojones /s", "lol", "ok"]))
+		message.channel.send(utils.random_from_list(
+			["jaja qué gracioso", "comedy heaven", "me parto los cojones /s", "la comedia fue hecha",
+			"Hey guys, did you know that in terms of human companionship, Flareon is objectively the most huggable Pokemon? While their maximum temperature is likely too much for most, they are capable of controlling it, so they can set themselves to the perfect temperature for you. Along with that, they have a lot of fluff, making them undeniably incredibly soft to touch. But that's not all, they have a very respectable special defense stat of 110, which means that they are likely very calm and resistant to emotional damage. Because of this, if you have a bad day, you can vent to it while hugging it, and it won't mind. It can make itself even more endearing with moves like Charm and Baby Doll Eyes, ensuring that you never have a prolonged bout of depression ever again.",
+			"lol", "ok"]))
 		ret = true
 	}
 	return ret
