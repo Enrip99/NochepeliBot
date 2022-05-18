23ebff637c6ba395af92b4acbf2e76aa19f14935
zhant <zhantyzgz@gmail.com>
2022-05-18 17:20:09 +0200

    opción autoupdate en /listall, desactivada por def

--- src/commands/listall.js
+++ src/commands/listall.js
@@ -9,11 +9,19 @@ const DESCRIPTION_LIMIT = 4096
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('listall')
-		.setDescription('lista todas las páginas de todas las pelis'),
+		.setDescription('lista todas las páginas de todas las pelis')
+		.addIntegerOption(option =>
+			option.setName('autoupdate')
+				.setDescription('Si la lista se actualiza automáticamente o no (default)')
+				.setRequired(false)
+				.addChoices({name: 'No actualizar', value: 0}, 
+							{name: 'Autoactualizar (CUIDAO QUE ESTO HACE QUE LA LISTA ANTERIOR DEJE DE ACTUALIZARSE)', value: 1})),
 	/** 
 	 * @param {import("discord.js").CommandInteraction} interaction
 	 */
 	async execute(interaction) {
+
+		let autoupdate = !!interaction.options.getInteger('autoupdate') //booleano
 		
 		if (!FilmManager.instance.count()) {
 			interaction.reply({ content: "No hay películas en la lista", ephemeral: true })
@@ -24,14 +32,16 @@ module.exports = {
 		let msg = await interaction.reply({ embeds: embeds, fetchReply: true })
 		if(!(msg instanceof DiscordMessage)) return
 
-		if(FilmManager.instance.list_renderer.pinned_message != null) {
-			interaction.channel.send("A partir de ahora estaré actualizando esta lista. Además, dejaré de actualizar la lista anterior")
-		} else {
-			interaction.channel.send("A partir de ahora estaré actualizando esta lista")
+		if(autoupdate){
+			if(FilmManager.instance.list_renderer.pinned_message != null) {
+				interaction.followUp("A partir de ahora estaré actualizando esta lista. Además, dejaré de actualizar la lista anterior")
+			} else {
+				interaction.followUp("A partir de ahora estaré actualizando esta lista")
+			}
+	
+			FilmManager.instance.list_renderer.pinned_message = Message.from(msg)
+			FilmManager.instance.save()					
 		}
-
-		FilmManager.instance.list_renderer.pinned_message = Message.from(msg)
-		FilmManager.instance.save()		
 
 		
 	}
